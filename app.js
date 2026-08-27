1f719271254f158379f43b2503c834612a219e84
mrnokmt <marin.okamoto@persol.co.jp>
2026-08-27 18:26:44 +0900

    0827進捗提出・ステータス表示用関数作成、まだ機能せず

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 const form = document.querySelector("#searchForm");
 let keyword = form.elements.keyword;
+const searchButton = document.querySelector("#searchButton");
+const status = document.querySelector("p");
+let searching = false;
+
+// ステータスにあわせた表示を行うための関数を作成
+const searchStatus = (state) => {
+  // 読み込み中の時
+  if (state === "loading") {
+    searching = true;
+    status.textContent = "検索中";
+    searchButton.disabled = "true";
+    return;
+  }
+  searching = false;
+  searchButton.disable = "false";
+
+  if (state === "finish") {
+    status.textContent = "";
+  } else if ((state = "null")) {
+    status.textContent = "キーワードを入力してください";
+  } else if (state === "noResult") {
+    status.textContent = "検索結果は0件です。";
+  } else if (state === "error") {
+    status.textContent = "エラーが発生しました。";
+  }
+};
 
 const reset = () => {
   keyword.value = "";
@@ -9,12 +35,14 @@ const reset = () => {
   }
 };
 
+// if(=== "loading"){}
 // 検索パネル
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  searchStatus();
   const searchWord = keyword.value.trim();
   if (searchWord === null || searchWord === "") {
-    alert("検索キーワードを入力してください");
+    searchStatus("null");
   } else {
     res(searchWord);
     console.log("submit finish");
@@ -46,16 +74,15 @@ const res = async (keyword) => {
       // 検索結果に応じて分岐を作成。jsonの中身は配列の形で返ってきている。
       if (data.length !== 0) {
         console.log("こちらがjsonの中身である検索結果一覧です", data);
-      　const ul = document.createElement("ul");
-      　ul.id = "ul";
-      　console.log("ul作成完了");
-      　const resultPanel = document.querySelector("#resultPanel");
-      　resultPanel.append(ul);
-      　// ulの中に情報を描画する関数を、配列の各要素に処理を適用するforEachで呼び出し。
-      　data.forEach(addCard);
+        const ul = document.createElement("ul");
+        ul.id = "ul";
+        console.log("ul作成完了");
+        const resultPanel = document.querySelector("#resultPanel");
+        resultPanel.append(ul); // ulの中に情報を描画する関数を、配列の各要素に処理を適用するforEachで呼び出し。
+        data.forEach(addCard);
         // 配列の中身がゼロ件ならば
       } else {
-        alert("検索結果は0件です");
+        searchStatus("noResult");
       }
     } else {
       // 業務上ではユーザーにどう見せるかなど。分析にも利用するので、「このAPIでエラーが出ました」など。
@@ -64,7 +91,12 @@ const res = async (keyword) => {
     }
     // そもそも、APIにアクセスできない場合のエラーメッセージを表示
   } catch {
-    alert("通信エラーが発生しました。");
+    console.log(error);
+    searchStatus("error");
+    //tryに入っても、catchに入っても共通で行う処理をfinallyに記載
+  } finally {
+    searching = "false";
+    searchButton.disabled = "false";
   }
 };
 
